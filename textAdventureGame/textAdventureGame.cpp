dde792c69125bf16ab6d5ec9d3083c13a70381a6
Oksana Kovalenko <ok.oksana.kovalenko@gmail.com>
2018-07-19 19:39:24 +0300

    Add game loop to Text Adventure Game

--- textAdventureGame/textAdventureGame.cpp
+++ textAdventureGame/textAdventureGame.cpp
@@ -19,10 +19,67 @@ void welcomePlayer(Player& player)
     cout << endl << "Hello " << playerName << endl;
 }
 
+void givePlayerOptions()
+{
+    cout <<
+        "What would you like to do? (Enter a corresponding number)"
+        << endl << endl;
+    cout << "1: Quit" << endl << endl;
+}
+
+void getPlayerInput(string& playerInput)
+{
+    cin >> playerInput;
+}
+
+enum class PlayerOptions
+{
+    Quit,
+    None
+};
+
+PlayerOptions evaluateInput(string& playerInput)
+{
+    PlayerOptions chosenOption = PlayerOptions::None;
+
+    if (playerInput.compare("1") == 0)
+    {
+        cout << "You have chosen to Quit!" << endl << endl;
+        chosenOption = PlayerOptions::Quit;
+    }
+    else
+    {
+        cout << "I do not recognize that option, try again!"
+            << endl << endl;
+    }
+
+    return chosenOption;
+}
+
+bool runGame()
+{
+    bool shouldEnd = false;
+
+    givePlayerOptions();
+
+    string playerInput;
+    getPlayerInput(playerInput);
+
+    shouldEnd = evaluateInput(playerInput) == PlayerOptions::Quit;
+
+    return !shouldEnd;
+}
+
 int main()
 {
     Player player;
     welcomePlayer(player);
+
+    bool isPlaying = true;
+    while (isPlaying)
+    {
+        isPlaying = runGame();
+    }
 
     return 0;
 }
